1e7c702c4da98416b822781eaed2afad22f2ba20
Roman Popat <roman@datakitchen.co.uk>
2024-07-28 18:32:13 +0000

    location check modal on app load

--- bs3/src/App.js
+++ bs3/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import BottomAppBar from './components/bottom-app-bar';
 import { Outlet } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setLocation } from './app/geolocatorSlice';
+import useGeoLocationCheck from './api/geolocation';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const geoLocation = useGeoLocationCheck();
+  dispatch(setLocation(geoLocation));
 
   return (
     <div className="App">
--- bs3/src/AppWrapper.js
+++ bs3/src/AppWrapper.js
@@ -1,21 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { setLocation } from './app/geolocatorSlice';
-import useGeoLocationCheck from './api/geolocation';
 import App from './App';
+
+const Modal = ({ onClose }) => (
+    <div className="App">
+        <div className="modal-content">
+            <h2>Welcome to the BS3 community.</h2>
+            <p>This app will ask to use your location data to confirm that you are in BS3.</p>
+            <p>It's optional but without it you will not be able to vote or post.</p>
+            <p>You are free to remain anonymous and your location data will never be stored.</p>
+            <button onClick={onClose}>OK</button>
+        </div>
+    </div>
+);
 
 export default function AppWrapper() {
     const dispatch = useDispatch();
     const [loading, setLoading] = useState(true);
-    const geoLocation = useGeoLocationCheck();
+    const [showModal, setShowModal] = useState(true);
 
     useEffect(() => {
-        dispatch(setLocation(geoLocation));
-        setLoading(false);
-    }, [dispatch, setLoading, geoLocation]);
+        if (!showModal) {
+            setLoading(false);
+        }
+    }, [dispatch, showModal]);
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    if (showModal) {
+        return <Modal onClose={handleCloseModal} />;
+    }
 
     if (loading) {
-        return <div>Loading...</div>; // Render a loading state while checking geolocation
+        return <div>Checking location...</div>; 
     }
 
     return <App />;
