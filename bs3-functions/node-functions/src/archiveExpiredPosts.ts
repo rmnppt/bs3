20a158cdd8db2d22ede22333fdaa0eeee4ae86eb
Roman Popat <roman.popat@proton.me>
2026-01-27 06:02:19 +0000

    fixed issue with api key

--- bs3-functions/node-functions/src/archiveExpiredPosts.ts
+++ bs3-functions/node-functions/src/archiveExpiredPosts.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+if (!admin.apps.length) admin.initializeApp();
 import { onSchedule } from "firebase-functions/v2/scheduler";
 
 const firestore = admin.firestore();
